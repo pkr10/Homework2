cd1535d347ab55699c4546e74773f6827919f735
박경록 <pkr10@naver.com>
2017-03-21 18:07:33 +0900

    최종수정본

--- app/src/main/java/com/example/rok/myapplication/Main8Activity.java
+++ app/src/main/java/com/example/rok/myapplication/Main8Activity.java
@@ -9,75 +9,79 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Main8Activity extends AppCompatActivity {
+public class Main8Activity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
-        final EditText e1,e2,e3;
-        final TextView t1,t2;
-        Button b1,b2;
-        t1 = (TextView)findViewById(R.id.st1);
-        t2 = (TextView)findViewById(R.id.st2);
-        e1 = (EditText)findViewById(R.id.se1);
-        e2 = (EditText)findViewById(R.id.se2);
-        e3 = (EditText)findViewById(R.id.se3);
-        b1 = (Button)findViewById(R.id.sb1);
-
+        final EditText e1, e2, e3;
+        final TextView t1, t2;
+        t1 = (TextView) findViewById(R.id.st1);
+        t2 = (TextView) findViewById(R.id.st2);
+        e1 = (EditText) findViewById(R.id.se1);
+        e2 = (EditText) findViewById(R.id.se2);
+        e3 = (EditText) findViewById(R.id.se3);
+        Button b1, b2;
         final ImageView i;
-
-        i = (ImageView)findViewById(R.id.imageView);
-
+        b1 = (Button)findViewById(R.id.sb1);
+        b2 = (Button) findViewById(R.id.sb2);
+        i = (ImageView) findViewById(R.id.imageView);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String string1 = e1.getText().toString();
                 String string2 = e2.getText().toString();
                 String string3 = e3.getText().toString();
-                int kor = Integer.parseInt(string1);
-                int math = Integer.parseInt(string2);
-                int eng = Integer.parseInt(string3);
-                final int sum  = kor+math+eng;
-                final double ave = (double)(sum/3);
 
-                t1.setText(sum+"점");
-                t2.setText((int) ave+"점");
+                if (string1.matches("") || string2.matches("") || string3.matches("")) {
+                    Toast.makeText(Main8Activity.this, "숫자를 입력해주세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    int kor = Integer.parseInt(string1);
+                    int math = Integer.parseInt(string2);
+                    int eng = Integer.parseInt(string3);
+                    final int sum = kor + math + eng;
+                    final double ave = (double) (sum / 3);
+
+                    t1.setText(sum + "점");
+                    t2.setText((int) ave + "점");
 
 
-                if(ave >90){
-                    i.setImageResource(R.drawable.a);
+                    if (ave > 90) {
+                        i.setImageResource(R.drawable.a);
+                    } else if (ave > 80) {
+                        i.setImageResource(R.drawable.b);
+                    } else if (ave > 70) {
+                        i.setImageResource(R.drawable.c);
+
+                    } else if (ave > 60) {
+                        i.setImageResource(R.drawable.d);
+                    } else {
+                        i.setImageResource(R.drawable.f);
+
+                    }
+
+
                 }
-                else if(ave>80){
-                    i.setImageResource(R.drawable.b);
-                }
-                else if(ave>70){
-                    i.setImageResource(R.drawable.c);
-
-                }
-                else if(ave>60){
-                    i.setImageResource(R.drawable.d);
-                }
-                else{
-                    i.setImageResource(R.drawable.f);
-
-                }
-
             }
         });
-
-        b2 = (Button)findViewById(R.id.sb2);
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                t1.setText("0");
-                t2.setText("0");
-                Toast.makeText(Main8Activity.this, "초기화되었습니다", Toast.LENGTH_SHORT).show();
+                t1.setText("0점");
+                t2.setText("0점");
                 i.setImageResource(0);
-
+                e1.setText(null);
+                e2.setText(null);
+                e3.setText(null);
 
             }
         });
-
     }
 }
+
+
+
+
+
+
--- app/src/main/java/com/example/rok/myapplication/Main9Activity.java
+++ app/src/main/java/com/example/rok/myapplication/Main9Activity.java
@@ -1,12 +1,15 @@
 package com.example.rok.myapplication;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.Switch;
@@ -23,8 +26,8 @@ public class Main9Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main9);
         final Switch s1;
         final Button b1,b2;
-        b1 = (Button)findViewById(R.id.resb1);
-        b2 = (Button)findViewById(R.id.resb2);
+        b1 = (Button)findViewById(R.id.resb2);
+        b2 = (Button)findViewById(R.id.resb1);
         s1 = (Switch)findViewById(R.id.ress1);
         final DatePicker d1;
         final TimePicker t1;
@@ -38,55 +41,170 @@ public class Main9Activity extends AppCompatActivity {
         e3 = (EditText)findViewById(R.id.rese3);
         final GridLayout g1;
         g1 = (GridLayout)findViewById(R.id.resg);
-        final TextView t2,t3,t4,t5,t6;
+        final TextView t2,t3,t4,t5,t6,t7;
         t2 = (TextView)findViewById(R.id.rest1);
         t3 = (TextView)findViewById(R.id.rest2);
         t4 = (TextView)findViewById(R.id.rest3);
         t5 = (TextView)findViewById(R.id.rest4);
         t6 = (TextView)findViewById(R.id.rest5);
+        final FrameLayout f1;
+        t7 = (TextView)findViewById(R.id.rest6);
+        final int[] count = {0};
+        final Chronometer c112;
+        c112 = (Chronometer)findViewById(R.id.resc1);
+        f1 = (FrameLayout)findViewById(R.id.resf);
         s1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(s1.isChecked()){
+                    c112.setVisibility(View.VISIBLE);
+                    t7.setVisibility(View.VISIBLE);
+                    f1.setVisibility(View.VISIBLE);
+                    g1.setVisibility(View.INVISIBLE);
                     d1.setVisibility(View.VISIBLE);
+                    d1.
+                    t1.setVisibility(View.INVISIBLE);
+                    l1.setVisibility(View.INVISIBLE);
+
+                    c112.start();
                     b1.setVisibility(View.VISIBLE);
+                    b1.setEnabled(false);
                     b2.setVisibility(View.VISIBLE);
                     final int year = d1.getYear();
-                    final int day = d1.getMonth();
-                    final int month = d1.getDayOfMonth();
+                    final int month = d1.getMonth();
+                    final int day = d1.getDayOfMonth();
+                    final int hour = t1.getCurrentHour();
+                    final int min = t1.getCurrentMinute();
+
                     b2.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            d1.setVisibility(View.INVISIBLE);
-                            t1.setVisibility(View.VISIBLE);
-                            final int hour = t1.getHour();
-                            final int min = t1.getMinute();
-                            b2.setOnClickListener(new View.OnClickListener() {
+                            count[0]++;
+                            if(count[0]==1){
+                                b1.setEnabled(true);
+                                b1.setVisibility(View.VISIBLE);
+                                d1.setVisibility(View.INVISIBLE);
+                                t1.setVisibility(View.VISIBLE);
+                                l1.setVisibility(View.INVISIBLE);
+                                g1.setVisibility(View.INVISIBLE);
+                                b2.setEnabled(true);
+
+
+                            }
+                            else if(count[0]==2){
+                                b2.setEnabled(true);
+
+                                b1.setEnabled(true);
+                                d1.setVisibility(View.INVISIBLE);
+                                g1.setVisibility(View.INVISIBLE);
+                                t1.setVisibility(View.INVISIBLE);
+                                l1.setVisibility(View.VISIBLE);
+                            }
+                            else if(count[0]==3){
+                                b1.setEnabled(true);
+                                d1.setVisibility(View.INVISIBLE);
+                                t1.setVisibility(View.INVISIBLE);
+                                l1.setVisibility(View.INVISIBLE);
+                                g1.setVisibility(View.VISIBLE);
+                                b2.setEnabled(false);
+                                t2.setText(year+"년"+month+"월"+day+"일");
+                                t3.setText(hour+"시"+min+"분");
+                                if(e1.getText().toString().matches("")){
+                                    t4.setText("");
+                                }
+                                else if(e2.getText().toString().matches("")){
+                                    t5.setText("");
+                                }
+                                else if(e3.getText().toString().matches("")){
+                                    t6.setText("");
+                                }
+                                else {
+
+
+                                    t4.setText(e1.getText() + "명");
+                                    t5.setText(e2.getText() + "명");
+                                    t6.setText(e3.getText() + "명");
+                                }
+                            }
+                            b1.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    l1.setVisibility(View.VISIBLE);
-                                    t1.setVisibility(View.INVISIBLE);
-                                    final String string1 = e1.getText().toString();
-                                    final String string2 = e2.getText().toString();
-                                    final String string3 = e3.getText().toString();
-                                    b2.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            l1.setVisibility(View.INVISIBLE);
-                                            g1.setVisibility(View.VISIBLE);
-                                            t2.setText(year+"년"+month+"월"+day+"일");
-                                            t3.setText(hour+"시"+min+"분");
-                                            t4.setText(string1+"명");
-                                            t5.setText(string2+"명");
-                                            t6.setText(string3+"명");
+                                    count[0]--;
+                                    if(count[0]==1){
+                                        b1.setEnabled(true);
+                                        b1.setVisibility(View.VISIBLE);
+                                        d1.setVisibility(View.INVISIBLE);
+                                        t1.setVisibility(View.VISIBLE);
+                                        l1.setVisibility(View.INVISIBLE);
+                                        g1.setVisibility(View.INVISIBLE);
+                                        b2.setEnabled(true);
 
+                                    }
+                                    else if(count[0]==2){
+                                        b2.setEnabled(true);
+
+                                        b1.setEnabled(true);
+                                        d1.setVisibility(View.INVISIBLE);
+                                        g1.setVisibility(View.INVISIBLE);
+                                        t1.setVisibility(View.INVISIBLE);
+                                        l1.setVisibility(View.VISIBLE);
+
+                                    }
+                                    else if(count[0]==3){
+                                        b1.setEnabled(true);
+                                        d1.setVisibility(View.INVISIBLE);
+                                        t1.setVisibility(View.INVISIBLE);
+                                        l1.setVisibility(View.INVISIBLE);
+                                        g1.setVisibility(View.VISIBLE);
+                                        b2.setEnabled(false);
+                                        t2.setText(year+"년"+month+"월"+day+"일");
+                                        t3.setText(hour+"시"+min+"분");
+                                        if(e1.getText().toString().matches("")){
+                                            t4.setText("");
                                         }
-                                    });
+                                        else if(e2.getText().toString().matches("")){
+                                            t5.setText("");
+                                        }
+                                        else if(e3.getText().toString().matches("")){
+                                            t6.setText("");
+                                        }
+                                        else {
+
+
+                                            t4.setText(e1.getText() + "명");
+                                            t5.setText(e2.getText() + "명");
+                                            t6.setText(e3.getText() + "명");
+                                        }
+                                    }
+                                    else if(count[0]==0){
+                                        d1.setVisibility(View.VISIBLE);
+                                        t1.setVisibility(View.INVISIBLE);
+                                        l1.setVisibility(View.INVISIBLE);
+                                        g1.setVisibility(View.INVISIBLE);
+                                        b1.setEnabled(false);
+                                        b2.setVisibility(View.VISIBLE);
+                                    }
                                 }
                             });
 
-                        }
-                    });
+
+
+                }
+            });
+
+
+                }
+                else{
+                    c112.setBase(SystemClock.elapsedRealtime());
+                    count[0]=0;
+                    c112.setVisibility(View.INVISIBLE);
+                    t7.setVisibility(View.INVISIBLE);
+                    f1.setVisibility(View.INVISIBLE);
+                    b1.setVisibility(View.INVISIBLE);
+                    b2.setVisibility(View.INVISIBLE);
+                    e1.setText(null);
+                    e2.setText(null);
+                    e3.setText(null);
                 }
             }
         });
